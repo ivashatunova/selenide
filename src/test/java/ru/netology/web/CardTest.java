398a3daf89f40f8ae4d44137c07998e7d98fd910
i.shatunova <ivashatunova@gmail.com>
2023-05-04 09:29:26 +0300

    Added date

--- src/test/java/ru/netology/web/CardTest.java
+++ src/test/java/ru/netology/web/CardTest.java
@@ -8,24 +8,29 @@ import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.*;
 
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
 public class CardTest {
+
+
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd. MM. yyyy");
+    LocalDateTime now = LocalDateTime.now();
+    LocalDateTime next = now.plusDays(5);
+    String data = dtf.format(now);
+
+
     @Test
     void shouldSendForm() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[data-test-id='city'] .input__control").setValue("Казань");
-        $("[data-test-id='date'] .input__control").setValue("05.07.2023");
+        $("[data-test-id='date'] .input__control").setValue(data);
         $("[data-test-id='name'] .input__control").setValue("Шатунова Иванна");
         $("[data-test-id='phone'] .input__control").setValue("+79168580321");
         $("[data-test-id='agreement'] .checkbox__box").click();
         $("button .button__text").click();
         $x("//div[contains(text(), 'Успешно')]").shouldBe(Condition.visible, Duration.ofSeconds(15));
-
-
-
-
-
-
 
 
     }
